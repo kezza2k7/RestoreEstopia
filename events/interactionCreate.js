b4af45d0fe8e65e7f9eefa57f42d764f65ebac58
Jay (Oreo 3) <jaygithub@estopia.net>
2024-09-23 03:42:34 +0100

    Added even more error handaler

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -48,10 +48,14 @@ module.exports = {
                 await command.execute(interaction);
             } catch (error) {
                 console.error(error);
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-                } else {
-                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                try {
+                    if (interaction.replied || interaction.deferred) {
+                        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+                    } else {
+                        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                    }
+                }   catch (error) {
+                    console.error('Error sending error message:', error);
                 }
             }
         } else if (interaction.isAutocomplete()) {
